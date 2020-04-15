c923ed2001677f51132379c5c76f2c5517bf336b
Jing Han <halgion@outlook.com>
2020-04-15 21:27:17 +0800

    update icon

--- questions.py
+++ questions.py
@@ -2,13 +2,15 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QDir
 from mainwindow import * # 主窗口
 
 if __name__ == '__main__':
     # Many browsers for security reason disable the loading of local files, but you can enable that capability with:
     sys.argv.append("--disable-web-security")
     app = QApplication(sys.argv)
-    app.setWindowIcon(QIcon("./icon.svg"))
+    iconpath = QDir.currentPath() + r'/icon.ico'
+    app.setWindowIcon(QIcon(iconpath))
     main_ui = MainWindow()
     main_ui.show()
     sys.exit(app.exec_())
